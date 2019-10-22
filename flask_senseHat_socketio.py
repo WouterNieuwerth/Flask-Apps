d67a6cb1e95328b6062ae819f5a80b7cf3c07b0c
Wouter Nieuwerth <wouternieuwerth@gmail.com>
2019-10-22 20:40:47 +0200

    Update flask_senseHat_socketio.py

--- flask_senseHat_socketio.py
+++ flask_senseHat_socketio.py
@@ -8,22 +8,23 @@ Created on Tue Oct 22 19:30:12 2019
 
 from flask import Flask
 from flask_socketio import SocketIO
-import time
-from sense_hat import SenseHat
-sense = SenseHat()
-
-sense.set_rotation(180)
+import threading
+#from sense_hat import SenseHat
+#sense = SenseHat()
+#
+#sense.set_rotation(180)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'paper motion'
 socketio = SocketIO(app)
 
-output = {
-    'pressure': sense.get_pressure(),
-    'temp_hum': sense.get_temperature_from_humidity(),
-    'tmp_press': sense.get_temperature_from_pressure(),
-    'humidity': sense.get_humidity()
-}
+#output = {
+#    'pressure': sense.get_pressure(),
+#    'temp_hum': sense.get_temperature_from_humidity(),
+#    'tmp_press': sense.get_temperature_from_pressure(),
+#    'humidity': sense.get_humidity()
+#}
+output = 'testtesttest'
 
 @app.route('/')
 def home():
@@ -31,7 +32,6 @@ def home():
 
 @app.route('/api/temp')
 def temp():
-    print('message was received!')
     return 'test 123'
 
 def messageReceived(methods=['GET', 'POST']):
@@ -41,10 +41,14 @@ def messageReceived(methods=['GET', 'POST']):
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
     socketio.emit('my response', json, callback=messageReceived)
-    
-while True:
+
+def timer():
+    threading.Timer(5.0, timer).start()
     socketio.emit('senseHat', {'data': output})
-    time.sleep(1)
+    #print('ticker')
+
+timer()
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
